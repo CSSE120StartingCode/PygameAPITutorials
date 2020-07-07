893921ddefdba65d9adfd464eb863ac978d7b0d0
David Fisher <fisherds@rose-hulman.edu>
2020-07-07 12:07:20 -0400

    refactor TTT starting code

--- 05-TicTacToe/TicTacToe.py
+++ 05-TicTacToe/TicTacToe.py
@@ -30,27 +30,29 @@ def get_xy_position(row, col):
 class Game:
     def __init__(self):
         # TODO: Create an empty board, called board
-        #         A list that contains 3 lists, each of those lists has 3 '.' values.
-        # TODO: Create a turn_counter set to 0 and a game_is_over set to False
+        #         A list that contains 3 lists, each of those lists has 3 "." values.
+        # TODO: Create a game_state_string set to X's turn
+        # TODO: Create a turn_counter variable set to 0
+        # TODO: Create a game_is_over variable set to False
         pass
 
     def take_turn(self, row, col):
         """Handle the current turn of the player and update board array"""
         # TODO: Check if game_is_over and return from this method (doing nothing) if True
         # TODO: Check if the value for row and col are valid.  Return (doing nothing) if invalid.
-        # TODO: Check if the mark at the requested row col is '.'.  Return (doing nothing) if it is not '.'
+        # TODO: Check if the mark at the requested row col is ".".  Return (doing nothing) if it is not "."
 
         # TODO: Determine if it is X's turn or O's turn (even turn_counter means X's turn, odd for O's turn)
-        # TODO: Modify the board by setting the current row col to an 'X' or an 'O' as appropriate
-        # TODO: Update the display caption (top title) text as appropriate "O's Turn" or "X's Turn"
+        # TODO: Modify the board by setting the current row col to an "X" or an "O" as appropriate
+        # TODO: Update the game_state_string as appropriate "O's Turn" or "X's Turn"
 
         # TODO: Increment the turn_counter
-        # TODO: If the turn_counter is 9 then the game is over
-        # TODO:        Update the game_is_over value and set the display caption to "Tie Game"
 
         self.check_for_game_over()
 
     def check_for_game_over(self):
+        # TODO: If the turn_counter is 9 then the game is over
+        # TODO:    If >=9 update the game_is_over value and set the game_state_string to "Tie Game"
         lines = []
         lines.append(self.board[0][0] + self.board[0][1] + self.board[0][2])
         lines.append(self.board[1][0] + self.board[1][1] + self.board[1][2])
@@ -62,16 +64,41 @@ class Game:
         lines.append(self.board[0][2] + self.board[1][1] + self.board[2][0])
 
         # TODO: Use the lines list to determine if there is a winner.
-        # TODO: If there is a winner, update the title text, play a sound, and set game_is_over to True.
+        # TODO: If there is a winner, update the game_state_string, play a sound, and set game_is_over to True.
 
 
-# --------------------------- Update the view ---------------------------
+# --------------------------- View Controller ---------------------------
 
+class ViewController(object):
 
-def draw_board(screen, game):
-    """ Draw the board based on the marked store in the board configuration array """
-    # TODO 3: Loop over the game.board to place X and O images on the screen as appropriate.
+    def __init__(self, screen):
+        """ Creates the view controller (the Tic-Tac-Toe game you see) """
+        # TODO: Initialize the ViewController, as follows:
+        # TODO    - Store the screen.
+        # TODO    - Create the game model object.
+        # TODO    - Create images for the board, X, and O images filenames.
+        # TODO  Use instance variables:   screen game board_image x_image o_image
+        pass
 
+    def check_event(self, event):
+        """ Takes actions as necessary based on the current event. """
+        # TODO: If the event is pygame.MOUSEBUTTONUP
+        #     Get the mouse click position as x and y variables
+        #     Convert the x and y variables into row and col using get_row_col
+        #     Inform the model object about this event
+        # TODO: If the event is pygame.KEYDOWN
+        #     Get the pressed_keys
+        #     If the key is pygame.K_SPACE, then reset the game.
+        pass
+
+    def draw(self):
+        """ Draw the board based on the marked store in the board configuration array """
+        # TODO: Blit the board_image onto the screen at the x y position of row=0 col=0
+        # TODO: Use a nested loop (via range) to go over all marks of the game.board
+        #    If the mark is "X", blit an X image at the x y position of row col
+        #    If the mark is "O", blit an O image at the x y position of row col
+        # TODO: Update the display caption to be the game.game_state_string
+        pass
 
 # --------------------------- Controller ---------------------------
 
@@ -80,27 +107,16 @@ def main():
     pygame.init()
     pygame.mixer.music.load("win.mp3")
     screen = pygame.display.set_mode((380, 400))
-    pygame.display.set_caption("X's Turn")
-    board_surface = pygame.image.load("board.png")
-    # TODO 1: Create an instance of the Game class
+    # TODO 1: Create an instance of the ViewController class called view_controller
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-            # TODO 6: When a MOUSEBUTTONUP event occurs take a game turn:
-            # TODO 7:   Get the X and Y mouse position
-            # TODO 8:   Use the get_row_col function to convert the XY mouse position into a row and column.
-            # TODO 9:   Call the take_turn method with the row and column values
-            # TODO 10: When the pygame.K_SPACE key is pressed create a new game instance and update the text.
+            # TODO : Pass the event to the view_controller
 
         screen.fill(pygame.Color("white"))
-        screen.blit(board_surface, get_xy_position(0, 0))
-
-        # TODO 2: Call draw_board
-        # TODO 4: As a temporary test hardcode a few 'X' or 'O' values in game.board
-        # TODO 5: Remove your temporary hardcoded tests
-
+        # TODO: Draw the view_controller
         pygame.display.update()
 
 
