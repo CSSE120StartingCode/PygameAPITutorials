1cd8813245ae92087d29713a111cf99d667a1a60
David Mutchler <mutchler@rose-hulman.edu>
2021-08-04 09:47:00 -0400

    Put back the simplest example, with no TODOs, that shows the game loop. Renamed the new version of that simplest example (from summer 2021 catapult), that has TODOs.  Left the third module's name unchanged in order to match the videos.

---
+++ 00-MovingSmile/00a-ExampleGameLoop.py
@@ -0,0 +1,22 @@
+import pygame
+import sys
+
+# Initialize the game, caption and screen:
+pygame.init()
+pygame.display.set_caption("Hello World")
+screen = pygame.display.set_mode((640, 480))
+
+# The game loop:
+while True:
+    screen.fill((255, 255, 255))  # white (full red, full green, full blue)
+
+    # Check for events (and respond to them as desired):
+    for event in pygame.event.get():
+        print(event)  # Used for an example here
+        if event.type == pygame.QUIT:
+            sys.exit()
+        # Additional interactions
+
+    # Draw things on the screen
+
+    pygame.display.update()
